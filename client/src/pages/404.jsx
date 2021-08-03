b331b15e7072fae15087d9573c443702b144b88b
JonMLunde <jonlund95@gmail.com>
2021-08-03 18:53:44 +0200

    404page

--- client/src/pages/404.jsx
+++ client/src/pages/404.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 export default function NotFound() {
-  return <div>404 NOT FOUND</div>;
+  return (
+    <div className="page-not-found">
+      <h1 className="page-not-found__title">404</h1>
+      <span className="page-not-found__subtitle">Sorry, but the requested page was not found</span>
+
+      <Link to="/">
+        <button className="page-not-found__button btn btn--white">Homepage</button>
+      </Link>
+    </div>
+  );
 }
